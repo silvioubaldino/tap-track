a7d48e8e43762b7de034dd4c6391fba6d0b114a2
Silvio Ubaldino de Sousa Neto <49573373+silvioubaldino@users.noreply.github.com>
2025-04-22 19:49:33 -0300

    Integrate with vercel analitics (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Layout from './components/Layout'
 import { TimeTracker } from './components/TimeTracker'
+import { VercelAnalytics } from './features/analytics'
 
 function App() {
   return (
-    <Layout>
-      <TimeTracker />
-    </Layout>
+    <>
+      <Layout>
+        <TimeTracker />
+      </Layout>
+      <VercelAnalytics />
+    </>
   )
 }
 
